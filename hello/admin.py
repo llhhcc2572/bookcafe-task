39448a8874aacd58ad9cef7fedc967a3d2a94bae
leehochul <llhhcc2572@naver.com>
2022-05-05 00:05:01 +0900

    데이터베이스 + 어드민

--- hello/admin.py
+++ hello/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import PossessingBook, WishBook
 # Register your models here.
+
+admin.site.register(PossessingBook)
+admin.site.register(WishBook)
--- hello/models.py
+++ hello/models.py
@@ -1,3 +1,39 @@
+import datetime
+from pydoc import describe
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+"""
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    def __str__(self):
+        return self.question_text
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
+    """
+class PossessingBook(models.Model) :
+    title = models.CharField(max_length=200)
+    link =  models.CharField(max_length=200)
+    image = models.ImageField()
+    author = models.CharField(max_length=200)
+    publisher = models.CharField(max_length=200)
+    isbn = models.CharField(max_length=200)
+    describe = models.CharField(max_length=500)
+    pubdate = models.DateTimeField('date published')
+
+class WishBook(models.Model):
+    title = models.CharField(max_length=200)
+    userid = models.CharField(max_length=200)
+    insertdate =models.DateTimeField('insertdate')
+    reqstatus = models.CharField(max_length=200)
+    statusmsg = models.CharField(max_length=200)
+    updatedate = models.DateTimeField('updatedate')
